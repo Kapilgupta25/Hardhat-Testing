ccf7256c6a89400bc85d73c6029505a2d22088f8
Kapil Gupta <kapilgpt25@gmail.com>
2024-06-11 14:54:04 +0530

    Complete testing

--- test/token.mjs
+++ test/token.mjs
@@ -15,27 +15,29 @@ describe("Token Contract", function () {
     });
   
     describe("Deployment", function () {
+
       it("Should set the right owner", async function () {
         expect(await hardhatToken.owner()).to.equal(owner.address);
       });
+
       it("Should assign the total supply of tokens to the owner", async function () {
         const totalSupplyPromise = hardhatToken.totalSupply();
         const ownerBalancePromise = hardhatToken.checkBalance(owner.address);
-
         const [totalSupply, ownerBalance] = await Promise.all([totalSupplyPromise, ownerBalancePromise]);
-
         expect(totalSupply.toString()).to.equal(ownerBalance.toString());
       });
     });
 
-
     describe("Transactions", function () {
+
         it("Should trasfer tokens between accounts", async function () {
+
           //owner account to address1.address
           await hardhatToken.transfer(address1.address, 10);
           let address1Balance = await hardhatToken.checkBalance(address1.address);
           expect(address1Balance.toString()).to.equal('10');
     
+          // address1.address to address2.address
           await hardhatToken.connect(address1).transfer(address2.address, 5);
           let address2Balance = await hardhatToken.checkBalance(address2.address);
           expect(address2Balance.toString()).to.equal('5');
@@ -45,6 +47,20 @@ describe("Token Contract", function () {
             const initialOwnerBalance = await hardhatToken.checkBalance(owner.address); //10000
             await expect(hardhatToken.connect(address1).transfer(owner.address, 1)).to.be.revertedWith("Not Enough Tokens");
             expect(await hardhatToken.checkBalance(owner.address)).to.equal(initialOwnerBalance);
+        });
+
+        it("Should update balances after transfers", async function () {
+            const initialOwnerBalance = await hardhatToken.checkBalance(owner.address);
+            await hardhatToken.transfer(address1.address, 5);
+            await hardhatToken.transfer(address2.address, 10);
+      
+            const finalOwnerBalance = await hardhatToken.checkBalance(owner.address);
+            expect(finalOwnerBalance.toNumber()).to.equal(initialOwnerBalance.toNumber() - 15);
+      
+            const address1Balance = await hardhatToken.checkBalance(address1.address);
+            expect(address1Balance.toNumber()).to.equal(5);
+            const address2Balance = await hardhatToken.checkBalance(address2.address);
+            expect(address2Balance.toNumber()).to.equal(10);
         });
     });
   });
